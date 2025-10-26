b7860e0581ab33126bf330e0437d318d888dadc2
hnowakowski <h.nowakowski.praca@gmail.com>
2025-10-26 17:43:18 +0100

    Cloudflare bypass (still a mess)

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -3,31 +3,32 @@ import requests
 import re
 from time import sleep
 from random import random
+import cloudscraper
 
 # can be some template pages if they ever pop up
 URL_BLACKLIST = []
 
 URL = "https://terraria.wiki.gg"
 
-def get_url(url, session):
+def get_url(url, scraper):
     headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-    "Referer": URL
+    "User-Agent": "Chrome/141.0.0.0"
     }
-    res = requests.get(url, headers=headers)
+    res = scraper.get(url, headers=headers)
     if res.status_code != 200:
         print(f"WARNING! Get request returned code other than 200: {res.status_code}")
-    return res
+    print(res.status_code)
+    return res.text
 
-def bfs(initial_pages, session, iterations):
+def bfs(initial_pages, iterations):
     all_pages = initial_pages[:]
     queue = initial_pages
     while iterations:
-        curr = get_url(URL + queue.pop(0).get("href"), session)
-        soup = bs4.BeautifulSoup(curr, "html.parser")
-        print(curr)
+        curr = get_url(URL + queue.pop(0).get("href"))
+        soup = bs4.BeautifulSoup(curr.text, "html.parser")
+        print(curr.text)
         with open("test.txt", "w") as f:
-            f.write(curr)
+            f.write(curr.text)
         body = soup.find("div", {"class": "mw-content-ltr mw-parser-output flash-anchored"})
         hyperlinks = [a for a in body.find_all("a", attrs={'href': re.compile(r'^/wiki')}) if not a.find("img")]
         for link in hyperlinks:
@@ -39,22 +40,19 @@ def bfs(initial_pages, session, iterations):
 
 
 
-
-
 def scrape():
-    session = requests.Session()
-    main_site = get_url(URL, session)
+    scraper = cloudscraper.CloudScraper()
+    main_site = get_url(URL, scraper)
     # id="main-section" <-- extract all wiki.gg hrefs from here and start bfs
     # h1 id="firstHeading" (page title) div class="mw-content-ltr mw-parser-output flash-anchored" (body) <--- for other pages
-    soup = bs4.BeautifulSoup(main_site.text, "html.parser")
+    soup = bs4.BeautifulSoup(main_site, "html.parser")
     body = soup.find("div", {"id": "main-section"})
     hyperlinks = [a for a in body.find_all("a", attrs={'href': re.compile(r'^/wiki')}) if not a.find("img")]
-    sleep(10*random())
     for link in hyperlinks:
-        print(link.text, ": ", link.get("href"))
-        get_url(URL + link.get("href"), session)
+        print(link, ": ", link.get("href"))
+        get_url(URL + link.get("href"), scraper)
         sleep(10*random())
     print("yeah")
     sleep(5)
-    all_pages = bfs(hyperlinks, session, 1)
+    all_pages = bfs(hyperlinks, 1)
 
